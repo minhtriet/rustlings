a05faef7c09e8907cad699ae106b0c5a8898e97c
minhtriet <minhtriet09@gmail.com>
2020-09-05 13:17:11 +0200

    add all exercises

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,11 +1,9 @@
 // variables2.rs
 // Make me compile! Execute the command `rustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
-    if x == 10 {
+    let x = 0.15 + 0.15 + 0.15;
+    if x == 0.1 + 0.25 + 0.1 {
         println!("Ten!");
     } else {
         println!("Not ten!");
